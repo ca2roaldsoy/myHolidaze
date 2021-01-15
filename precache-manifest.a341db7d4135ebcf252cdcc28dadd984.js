fba0bc711ae609080f0d7cb083b4855ae8aa2a6f
ca2roaldsoy <roaldsoyen@gmail.com>
2021-01-15 19:39:24 +0100

    Updates

---
+++ precache-manifest.a341db7d4135ebcf252cdcc28dadd984.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1950a1b7ad45ab41e220b4f691e4f0d",
+    "url": "/myHolidaze/index.html"
+  },
+  {
+    "revision": "37b969be043e8ef31ec2",
+    "url": "/myHolidaze/static/css/2.b30770e3.chunk.css"
+  },
+  {
+    "revision": "7cf5a0dc6d531b676515",
+    "url": "/myHolidaze/static/css/main.165efd74.chunk.css"
+  },
+  {
+    "revision": "37b969be043e8ef31ec2",
+    "url": "/myHolidaze/static/js/2.12b30c80.chunk.js"
+  },
+  {
+    "revision": "64f7e58d0b20d4d7a9a6188b47824605",
+    "url": "/myHolidaze/static/js/2.12b30c80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cf5a0dc6d531b676515",
+    "url": "/myHolidaze/static/js/main.19329f8a.chunk.js"
+  },
+  {
+    "revision": "5a139288fc44bf563209",
+    "url": "/myHolidaze/static/js/runtime-main.2dfb6d1a.js"
+  },
+  {
+    "revision": "8e674387cd68ad23ba5e5a714cdcc424",
+    "url": "/myHolidaze/static/media/Gloria.8e674387.ttf"
+  },
+  {
+    "revision": "af7c6eaa7d8e1c021f08bcb96d667287",
+    "url": "/myHolidaze/static/media/accept.af7c6eaa.png"
+  },
+  {
+    "revision": "ff9400da07becc3f678933218fbcbabf",
+    "url": "/myHolidaze/static/media/accept_white.ff9400da.png"
+  },
+  {
+    "revision": "350e948819d2cc0a82c426508c2a95df",
+    "url": "/myHolidaze/static/media/admin_page.350e9488.jpg"
+  },
+  {
+    "revision": "9935cac9832588b6c6d07bf962e10e6e",
+    "url": "/myHolidaze/static/media/bergen_blur_v4.9935cac9.jpg"
+  },
+  {
+    "revision": "500924b9576010b6e8e9a7ff481c873c",
+    "url": "/myHolidaze/static/media/bg-15.500924b9.jpg"
+  },
+  {
+    "revision": "d2ca8cc37227040f81b3ee7dd98e0008",
+    "url": "/myHolidaze/static/media/bryggen.d2ca8cc3.jpg"
+  },
+  {
+    "revision": "429de792d4b5a00f426689de83a21685",
+    "url": "/myHolidaze/static/media/fløibanen_v2.429de792.jpg"
+  },
+  {
+    "revision": "4f039f2f184480e2b59350bdc4841d8c",
+    "url": "/myHolidaze/static/media/information.4f039f2f.png"
+  },
+  {
+    "revision": "754085e8728c14766d5a7b04762b80c0",
+    "url": "/myHolidaze/static/media/reject_white.754085e8.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/myHolidaze/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/myHolidaze/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/myHolidaze/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "ae62be60d80b8b199d41391459c84280",
+    "url": "/myHolidaze/static/media/stop.ae62be60.png"
+  },
+  {
+    "revision": "491e1a0db0c696d58855cbf715959873",
+    "url": "/myHolidaze/static/media/trolltunga_v2.491e1a0d.jpg"
+  },
+  {
+    "revision": "81c2a708d09e266f4b926cff6ccf143e",
+    "url": "/myHolidaze/static/media/ulriken_v2.81c2a708.jpg"
+  }
+]);
